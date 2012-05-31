c14938f571b2e8cfdd75a0ff19218aff1b64e99d
Philip Cavaco <philipcavaco@gmail.com>
2012-05-31 01:01:51 +0100

    Initial Android App
    Completed the "Building your first App" lesson

---
+++ source/expense_tracker/src/com/cavaco/expensetracker/DisplayMessageActivity.java
@@ -0,0 +1,22 @@
+package com.cavaco.expensetracker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Expense_trackerActivity.EXTRA_MESSAGE);
+        
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        
+        setContentView(textView);
+    }
+
+}
